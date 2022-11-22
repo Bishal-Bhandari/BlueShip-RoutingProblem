7cf55f5b4aaaec3325f5b3cea2b40360820c3d21
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-11-22 15:40:21 +0100

    using plotly and geopands

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # BlueShipRoutingProblem
+
+Bishal Bhandari
+(University of Bamberg)
+1. Create a map of atlantic section.
+2. Plot the density of marine life in atlantic oceans.
+3. Use polygons or cluster point to present density of marine life.
+4. Point the ports or harbour.
+5. Plot the route using algorithm.
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-import matplotlib as mt
+import plotly.express as px
 import pandas as pd
-import geopandas as gpd
 
-world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
-x = world.plot()
-print(x)
+x = px.data.carshare()
+df = x.head(10)
+print(df)
+
+fig = px.scatter_mapbox(df,
+                        lon=df['centroid_lon'],
+                        lat=df['centroid_lat'],
+                        zoom=3,
+                        color=df['peak_hour'],
+                        size=df['car_hours'],
+                        width=1200,
+                        height=900,
+                        title='Blue Ship Routing')
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r": 0, "t": 50, "l": 0, "b": 10})
+
+fig.show()
+print("Plot.....")
+
