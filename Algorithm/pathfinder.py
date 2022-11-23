fc7cccb050495a00c6dc64fc45a67d922af32a1d
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-11-23 19:59:46 +0100

    Creating the structure

---
+++ Algorithm/pathfinder.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def fitness():
+    return True
+
+
+def crossover(a, b):
+    return a
+
+
+def mutation(member):
+    return member
+
+
+def create_new_member(member):
+    return member
+
+
+def next_gen(population):
+    return population
+
+
+def main(number_of_iteration):
+    return True
