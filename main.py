2e4a827e708482290f4795b8b304785e1e53b24b
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-11-23 16:32:33 +0100

    plotted the data into mapbox

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import plotly.express as px
 import pandas as pd
 
-x = px.data.carshare()
-df = x.head(10)
+# Read data file
+df = pd.read_excel(r'SmallData.xlsx')
+
+# filter the data percentage to present
+df = df.sample(frac=0.5)
+
 print(df)
 
 fig = px.scatter_mapbox(df,
-                        lon=df['centroid_lon'],
-                        lat=df['centroid_lat'],
-                        zoom=3,
-                        color=df['peak_hour'],
-                        size=df['car_hours'],
+                        lon=df['Lon'],
+                        lat=df['Lat'],
+                        zoom=5,
+                        color=df['Whale_Type'],
+                        size=df['Population_Density'],
                         width=1200,
                         height=900,
                         title='Blue Ship Routing')
@@ -18,5 +22,4 @@ fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r": 0, "t": 50, "l": 0, "b": 10})
 
 fig.show()
-print("Plot.....")
-
+print("Plotting.....")
