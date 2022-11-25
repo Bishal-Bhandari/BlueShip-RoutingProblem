5805b47ea902e557ae9aa903b31ee2efb1f53fb6
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-11-25 22:51:25 +0100

    Initial genetic AI

--- Algorithm/pathfinder.py
+++ Algorithm/pathfinder.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
 Check_Condition = True
 # Plotting field
@@ -17,8 +18,8 @@ end_x = [8]
 end_y = [8]
 
 # Obstacles
-obs_x = [-5, 1, 8, 4, 5, 6, 8, 4, 5, 7, 8, 7, 2, 2, 4, 6]
-obs_y = [-5, 5, 8, 4, 4, 2, 3, 7, 2, 2, 8, 1, 8, 7, 3, 4]
+obs_x = [-5, 1, 8, 4, 5, 6, 8, 4, 8, 5, 7, 8, 7, 2, 2, 4, 6]
+obs_y = [-5, 5, 8, 4, 4, 2, 3, 7, 8, 2, 2, 8, 1, 8, 7, 3, 4]
 
 # plotting the points
 hold_x = []
@@ -26,10 +27,10 @@ hold_y = []
 
 
 def randomize():
-    return random.uniform(-1, 2)
+    return random.uniform(-1, 1), random.choice([1, 2])
 
 
-def initialization():
+def gene_computation():
     global Check_Condition
     i, j = 0, 0
 
@@ -39,24 +40,29 @@ def initialization():
         for idx in range(len(obs_x)):
             if start_x[i] == obs_x[idx] and start_y[i] == obs_y[idx]:
                 random_val = randomize()
-                if random_val == 1:
-                    start_x[i] = start_x[j] + random_val
+                if random_val[1] == 1:
+                    start_x[i] = start_x[i] + 1.5 * random_val[0]
                 else:
-                    start_y[i] = start_y[j] + random_val
+                    start_y[i] = start_y[j] + 1.5 * random_val[0]
+
+        if start_x[i] > end_x[0] or start_y[j] > end_y[0]:
+            start_x[i] = start_x[i] - (start_x[i] - end_x[0])
+            start_y[j] = start_y[j] - (start_x[j] - end_x[0])
 
         hold_x.append(start_x[i])
         hold_y.append(start_y[i])
-        if start_x[i] >= end_x[0] or start_y[j] >= end_y[0]:
+
+        if start_x[i] == end_x[0] or start_y[j] == end_y[0]:
             Check_Condition = False
             return hold_x, hold_y
+
         i = i + 1
         j = j + 1
 
 
 def plot_grid():
-    hold_x, hold_y = initialization()
-    print(f'x,{hold_x}')
-    print(f'y,{hold_y}')
+    hold_x, hold_y = gene_computation()
+    print(f'x{hold_x}-y{hold_y}')
     # Max field plot
     plt.plot(sfield_x, sfield_y, c='white')
     plt.plot(efield_x, efield_y, c='white')
@@ -64,6 +70,8 @@ def plot_grid():
 
     # Plotting points for scatter
     plt.scatter(obs_x, obs_y, label="stars", color=['red'])
+    # For goal point
+    plt.scatter(end_x, end_y, color=['Black'])
 
     # naming the x axis
     plt.xlabel('x - axis')
