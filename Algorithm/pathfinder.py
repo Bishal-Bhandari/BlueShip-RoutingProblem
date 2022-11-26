08f8e82986d137bba4cb16f33554dff2926bce84
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-11-26 10:17:31 +0100

    Catching the goal point done

--- Algorithm/pathfinder.py
+++ Algorithm/pathfinder.py
@@ -1,8 +1,11 @@
+import math
+
 import matplotlib.pyplot as plt
 import random
 import numpy as np
 
 Check_Condition = True
+
 # Plotting field
 sfield_x = [-10]
 sfield_y = [-10]
@@ -15,11 +18,11 @@ start_y = []
 
 # Goal point
 end_x = [8]
-end_y = [8]
+end_y = [14]
 
 # Obstacles
-obs_x = [-5, 1, 8, 4, 5, 6, 8, 4, 8, 5, 7, 8, 7, 2, 2, 4, 6]
-obs_y = [-5, 5, 8, 4, 4, 2, 3, 7, 8, 2, 2, 8, 1, 8, 7, 3, 4]
+obs_x = [-5, 1, 8, 4, 5, 6, 8, 4, 8, 5, 7, 8, 7, 2, 2, 4, 6, 9]
+obs_y = [-5, 5, 8, 4, 4, 2, 3, 7, 8, 2, 2, 8, 1, 8, 7, 3, 4, 9]
 
 # plotting the points
 hold_x = []
@@ -28,6 +31,13 @@ hold_y = []
 
 def randomize():
     return random.uniform(-1, 1), random.choice([1, 2])
+
+
+def fitness_fun(hol_x, hol_y):
+    Euclidean_distance = math.dist(hol_x, hol_y)
+    print(Euclidean_distance)
+
+    return True
 
 
 def gene_computation():
@@ -41,18 +51,22 @@ def gene_computation():
             if start_x[i] == obs_x[idx] and start_y[i] == obs_y[idx]:
                 random_val = randomize()
                 if random_val[1] == 1:
-                    start_x[i] = start_x[i] + 1.5 * random_val[0]
+                    start_x[i] = start_x[i] + 0.5 * random_val[0]
                 else:
                     start_y[i] = start_y[j] + 1.5 * random_val[0]
 
-        if start_x[i] > end_x[0] or start_y[j] > end_y[0]:
+        if start_x[i] > end_x[0]:
             start_x[i] = start_x[i] - (start_x[i] - end_x[0])
-            start_y[j] = start_y[j] - (start_x[j] - end_x[0])
+        elif start_y[j] > end_y[0]:
+            start_y[j] = start_y[j] - (start_y[j] - end_y[0])
+        else:
+            print("")
 
         hold_x.append(start_x[i])
         hold_y.append(start_y[i])
+        print(f'x{hold_x}-y{hold_y}')
 
-        if start_x[i] == end_x[0] or start_y[j] == end_y[0]:
+        if start_x[i] == end_x[0] and start_y[j] == end_y[0]:
             Check_Condition = False
             return hold_x, hold_y
 
@@ -62,15 +76,18 @@ def gene_computation():
 
 def plot_grid():
     hold_x, hold_y = gene_computation()
-    print(f'x{hold_x}-y{hold_y}')
-    # Max field plot
+
+    fitness_value = fitness_fun(hold_x, hold_y)
+
+    # Size of a field plot
     plt.plot(sfield_x, sfield_y, c='white')
     plt.plot(efield_x, efield_y, c='white')
+    # Line data holder
     plt.plot(hold_x, hold_y, c='black')
 
-    # Plotting points for scatter
+    # Plotting points for obstacle
     plt.scatter(obs_x, obs_y, label="stars", color=['red'])
-    # For goal point
+    # For end point of line
     plt.scatter(end_x, end_y, color=['Black'])
 
     # naming the x axis
